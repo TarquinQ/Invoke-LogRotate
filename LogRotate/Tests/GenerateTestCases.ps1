cbf7c9e8feefb73e905c0ad7559a40912d5f5c33
Milton Yates <milton.yates@gmail.com>
2017-06-14 23:08:54 +0930

    Initial creation of Test Cases

--- LogRotate/Tests/GenerateTestCases.ps1
+++ LogRotate/Tests/GenerateTestCases.ps1
@@ -15,11 +15,44 @@
     At this stage, there is no seperated test case generation.
 
 .EXAMPLE
-    NB: Any files with the Test-Case filenames will be overwritten.
+    NB: Any files within the Test-Case diretory will be deleted.
     
     Optional Arguments:
     -Verbose    Enables extra debugging output   
-    -FullReset  This switch simply deletes all contents from the specified folder, and
-                recreates as a standardised test layout
 #>
+param (
+    [String]$Path=($PSScriptRoot + "\Test-Cases")
+)
+
+$ErrorActionPreference = "Continue"
+$Global:VerbosePreference = 'Continue'
+$Script:VerbosePreference = 'Continue'
+$VerbosePreference = 'Continue'
+
+
+Function RecreatePath {
+    param (
+        [Parameter(Mandatory=$True)][String]$Path,
+        [Parameter(Mandatory=$True)][Object[]]$SubDirArray
+    )
+    $prevErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = Stop
+    if (Test-Path $Path) { Remove-Item -Path $Path -Force }
+    New-Item -Path $Path -Force
+    $SubDirArray | % { New-Item -Path $_ -Force }
+    $ErrorActionPreference = $prevErrorActionPreference
+}
+
+
+Function CreateLargeItem {
+    param (
+        [Parameter(Mandatory=$True)][String]$Path,
+        [Parameter(Mandatory=$True)][String[]]$Filenames,
+        [Parameter(Mandatory=$True)][Int]$FileSizeKB
+    )
+    $ContentBase = ( -Join ((65..90) + (97..122) | Get-Random -Count 1024 | % {[char]$_}) )  # 1024 bytes of random chars
+    $FullContents = $ContentBase*$FileSizeKB
+    ForEach ($filename in $filenames) { New-Item -Path "${Path}\${Filename}" -type file -Force }
+}
+
 
